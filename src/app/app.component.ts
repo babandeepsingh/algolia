43a2b2df5742be8f62c7ffcd9463bf93473652b7
babandeepsingh <er.babandeep@gmail.com>
2020-07-18 10:50:52 +0530

    pagination updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService } from "./data.service";
+import { stringify } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,9 @@ export class AppComponent {
   loader = true;
   apiRes: any = {};
   hits = [];
+  name = "Angular";
+  paginationPages = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
   getSourceName = (url: string) => {
     return (url.split('//')[1].split('/')[0].split('.').splice(1).join('.'))
   }
@@ -23,5 +27,17 @@ export class AppComponent {
         this.apiRes = data;
         this.hits = this.apiRes.hits.filter(hits => hits.title != null && hits.url)
       });
+  }
+  hitApi = (page:number) => {
+    this.appService.sendGetRequestPage(this.name ? this.name : '', page)
+      .subscribe(data => {
+        this.loader = false
+        this.apiRes = data;
+        this.hits = this.apiRes.hits.filter(hits => hits.title != null && hits.url)
+      });
+  }
+  buttonPagination = (pagePassed: number) => {
+    this.loader = true
+    this.hitApi(pagePassed);
   }
 }
